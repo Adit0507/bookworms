34300022783483172a7cedb28041100358165279
Adit0507 <adi05072002@gmail.com>
2024-08-02 23:15:37 +0530

    workin...

--- bookworm.go
+++ bookworm.go
@@ -1,3 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Bookworm struct {
+	Name  string `json:"name"`
+	Books []Book `json:"books"`
+}
+
+type Book struct {
+	Author string `json:"author"`
+	Title  string `json:"title"`
+}
+
 func loadBookworms(filePath string) ([]Bookworm, error) {
-	return nil
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	var bookworms []Bookworm
+
+	err = json.NewDecoder(f).Decode(&bookworms)
+	if err != nil {
+		return nil, err
+	}
+
+	return bookworms, nil
 }
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main(){
-		
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	bookworms, err := loadBookworms("testdata/bookworms.json")
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "failed to load bookwroms:")
+		os.Exit(1)
+	}
+
+	fmt.Println(bookworms)
 }
